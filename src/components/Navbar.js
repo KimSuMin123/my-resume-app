9e09b9f071bec9d353bd2501d6becbb2118a8cd7
Kimsumin123 <tntn211@naver.com>
2024-09-02 15:34:09 +0900

    up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import './Navbar.css';
 
 function Navbar() {
-  return (
-    <nav className="navbar">
-      <ul className="navbar-menu">
-        <li className="navbar-item"><a href="#Home">홈</a></li>
-        <li className="navbar-item"><a href="#skills">기술</a></li>
-        <li className="navbar-item"><a href="#education">교육</a></li>
-        <li className="navbar-item"><a href="#experience">경력</a></li>
-        <li className="navbar-item"><a href="#awards">수상</a></li>
-        <li className="navbar-item"><a href="#volunteer">봉사</a></li>
-        <li className="navbar-item"><a href="#certifications">자격증</a></li>
-        <li className="navbar-item"><a href="#activities">활동</a></li>
-        <li className="navbar-item"><a href="#projects">프로젝트</a></li>
-      </ul>
-    </nav>
-  );
-}
-const navbarStyle = {
+  const navbarStyle = {
     backgroundColor: '#333',
     padding: '10px',
     fontFamily: "'Helvetica Neue', Arial, sans-serif",
+    position: 'fixed',    // 상단에 고정
+    top: '0',             // 화면의 최상단에 위치
+    width: '100%',        // 전체 너비 차지
+    zIndex: '1000',       // 다른 요소 위에 위치
   };
   
   const navbarMenuStyle = {
@@ -45,4 +32,22 @@ const navbarStyle = {
   const navbarLinkHoverStyle = {
     color: '#ddd',
   };
+
+  return (
+    <nav style={navbarStyle}>
+      <ul style={navbarMenuStyle}>
+        <li style={navbarItemStyle}><a style={navbarLinkStyle} href="#Home">홈</a></li>
+        <li style={navbarItemStyle}><a style={navbarLinkStyle} href="#skills">기술</a></li>
+        <li style={navbarItemStyle}><a style={navbarLinkStyle} href="#education">학력</a></li>
+        <li style={navbarItemStyle}><a style={navbarLinkStyle} href="#experience">경력</a></li>
+        <li style={navbarItemStyle}><a style={navbarLinkStyle} href="#awards">수상</a></li>
+        <li style={navbarItemStyle}><a style={navbarLinkStyle} href="#volunteer">봉사</a></li>
+        <li style={navbarItemStyle}><a style={navbarLinkStyle} href="#certifications">자격증</a></li>
+        <li style={navbarItemStyle}><a style={navbarLinkStyle} href="#activities">활동</a></li>
+        <li style={navbarItemStyle}><a style={navbarLinkStyle} href="#projects">프로젝트</a></li>
+      </ul>
+    </nav>
+  );
+}
+
 export default Navbar;
