5bc25a32256e3f9d0c2354f9842d86cf9ade8292
Kimsumin123 <tntn211@naver.com>
2024-10-15 00:44:43 +0900

    프로젝트 세부 내용 등록

--- src/components/Education.js
+++ src/components/Education.js
@@ -9,21 +9,21 @@ const Education = () => {
         <div style={educationItemStyle}>
           
           <h3 style={subHeadingStyle}>대림대학교(4년제)</h3>
-          <p style={dateStyle}>2024.03 ~ 재학중</p>
+          <p style={dateStyle}>2021.03 ~ 2025.02 / 졸업예정</p>
           <p style={detailStyle}>컴퓨터 소프트웨어 학과</p>
         </div>
         <div style={educationItemStyle}>
           
-          <h3 style={subHeadingStyle}>구름톤 트레이닝 수료</h3>
-          <p style={dateStyle}>수료(2024.02~2024.08)</p>
+          <h3 style={subHeadingStyle}>[구름 X 인프런] 자바 스프링 & 리액트 풀스택 개발자 과정_7회차</h3>
+          <p style={dateStyle}>수료(2024.02.02 ~ 2024.08.14)</p>
           <p style={detailStyle}>[구름 X 인프런] 자바 스프링 & 리액트 풀스택 개발자 과정</p>
+          <p style={detailStyle}>
+          교육내용<br/>
+            HTML / CSS / JavaScript / TypeScript / React.js <br/>
+            Java / Spring / Git
+          </p>
         </div>
-        <div style={educationItemStyle}>
-         
-          <h3 style={subHeadingStyle}>대림대학교(3년제)</h3>
-          <p style={dateStyle}>졸업(2021.03~2024.02)</p>
-          <p style={detailStyle}>컴퓨터정보학부</p>
-        </div>
+        
       </section>
     </div>
     </section>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,167 +1,265 @@
-import React from 'react';
-import code from '../img/code.png';  // 이미지 파일 경로에 맞게 수정하세요
-import ieum from '../img/ieum.png';  // 이미지 파일 경로에 맞게 수정하세요
+import React, { useState } from 'react';
+import club from '../img/club.png';
+import code1 from '../img/code1.png';
+import code2 from '../img/code2.png';
+import coktail from '../img/coktail.png';
+import dealim from '../img/dealim.png';
+import map from '../img/map.png';
+import mbti1 from '../img/mbti1.png';
+import mbti2 from '../img/mbti2.png';
+import me from '../img/me.png';
+import travel1 from '../img/travel1.png';
+import travel2 from '../img/travel2.png';
 
 const Projects = () => {
-  return (
-    <section id="projects">
-      <div style={containerStyle}>
-        {/* Projects Section */}
-        <section style={sectionStyle}>
-          <h3 style={headingStyle}>프로젝트</h3>
+  const [filter, setFilter] = useState('all'); // 프로젝트 필터 상태
 
-          <div style={projectsContainerStyle}>
-            <div style={activityItemStyle}>
-              <h4 style={subHeadingStyle}>2021년 대림대 지도 앱 제작 활동 참여</h4>
-              <p style={dateStyle}>2021.05 ~ 2021.10</p>
-              <p style={detailStyle}>대림대 지도 앱 개발 - 데이터 관리 및 디자인 업무</p>
-            </div>
+  const handleFilterChange = (type) => {
+    setFilter(type); // 버튼 클릭 시 필터 업데이트
+  };
 
-            <div style={activityItemStyle}>
-              <h4 style={subHeadingStyle}>2022년 대림대 해커톤</h4>
-              <p style={dateStyle}>2022.04 ~ 2022.07</p>
-              <p style={detailStyle}>동아리 통합 사이트 개발 - React를 이용하여 프론트 엔드 개발</p>
-            </div>
-          </div>
+  // 팀 프로젝트 목록
+  const teamProjects = [
+    {
+      title: '2021년 대림대 지도 앱 제작 활동 참여',
+      date: '2021.05 ~ 2021.10',
+      person: '5인',
+      projectname: '대림대 지도 앱 ',
+      detail: 'Mysql를 이용하여 데이터 관리, Figma를 이용하여 디자인 업무',
+      lan: 'Mysql, Figma, Android Studio',
+      plus: 'https://tnrn211.tistory.com/146',
+      images: [dealim]
+    },
+    {
+      title: '2022년 대림대 해커톤',
+      date: '2022.04 ~ 2022.07',
+      person: '4인',
+      projectname: '동아리 통합 사이트 ',
+      detail: 'Figma를 이용한 디자인 작업, React를 이용하여 프론트엔드 개발 (게시판 기능)',
+      lan: 'Figma, react.js, VSCode',
+      images: [club]
+    },
+    {
+      title: '2022년 대림대 테크 페어 장려상',
+      date: '2022.08 ~ 2022.11',
+      person: '6인',
+      projectname: 'MBTI 동아리 추천 사이트',
+      detail: '코틀린을 이용하여 서버 개발하기 (인적사항 조사, MBTI 검사, 지도 기능)',
+      lan: 'Android Studio, 코틀린',
+      plus: 'https://www.youtube.com/embed/YubAbuHyKdQ',
+      images: [mbti1, mbti2]
+    },
+    {
+      title: '2023년 대림대 해커톤',
+      date: '2023.04 ~ 2023.07',
+      person: '2인',
+      projectname: '랜덤 칵테일 추천 사이트',
+      detail: 'React를 이용하여 프론트엔드 전체 개발, Figma를 이용한 디자인 작업',
+      lan: 'Figma, React.js, VSCode',
+      plus: 'https://www.youtube.com/embed/S62rpAE_94I',
+      images: [coktail]
+    },
+    {
+      title: '2024년 대림대 해커톤',
+      date: '2024.04 ~ 2024.07',
+      person: '4인',
+      projectname: '코린이들을 위한 코딩 게임 (코드 어드벤처)',
+      detail: 'React 프론트엔드 개발 (스테이지 및 문제 풀이 기능), Node 백엔드 개발 (문제 풀이 기능)',
+      lan: 'Node.js, react.js, VSCode',
+      plus: 'https://www.canva.com/design/DAGJLifg0YI/0ruL7Qk2npEBFaPbBG6IEA/edit?utm_content=DAGJLifg0YI&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton',
+      images: [code1, code2]
+    },
+    {
+      title: '2024년 구름톤 트레이닝 프로젝트',
+      date: '2024.05 ~ 2024.08',
+      person: '6인',
+      projectname: '친구와 함께 여행 계획 웹 (이음)',
+      detail: 'React 프론트엔드 개발 (일정 생성, 편집 기능, 실시간 채팅 기능), Figma를 이용한 디자인 작업',
+      lan: 'React.js, Figma, VSCode',
+      plus: 'https://www.miricanvas.com/ko/v/13j287h',
+      images: [travel1, travel2]
+    },
+  ];
 
-          <div style={projectsContainerStyle}>
-            <div style={activityItemStyle}>
-              <iframe 
-                width="100%" 
-                height="350" 
-                src="https://www.youtube.com/embed/YubAbuHyKdQ" 
-                title="YouTube video player" 
-                frameBorder="0" 
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                allowFullScreen
-                style={iframeStyle}
-              ></iframe>
-              <h4 style={subHeadingStyle}>2022년 대림대 테크 페어 장려상</h4>
-              <p style={dateStyle}>2022.08 ~ 2022.11</p>
-              <p style={detailStyle}>MBTI 동아리 추천 사이트 개발 - Android Studio 서버 개발</p>
-            </div>
+  // 개인 프로젝트 목록
+  const personalProjects = [
+    {
+      title: '셔틀 예약 사이트 만들기',
+      projectname: '대림대 셔틀 예약 시스템',
+      date: '2024.08 ~ 2024.10',
+      detail: '효율적인 셔틀 관리를 위한 웹앱 - React 프론트 엔드 개발, Node 백엔드 개발',
+      lan: 'React.js, Node.js, VSCode',
+      plus: 'https://tnrn211.tistory.com/601',
+      person: '개인',
+      images: [map],
+    },
+    {
+      title: '이력서 사이트 만들기',
+      projectname: '개인 포트폴리오 및 이력서 사이트',
+      date: '2024.09 ~ ',
+      detail: '개인 포트폴리오와 이력서 사이트 - React 프론트 엔드 개발',
+      lan: 'React.js, Figma, VSCode',
+      person: '개인',
+      images: [me],
+    },
+  ];
 
-            <div style={activityItemStyle}>
-              <iframe 
-                width="100%" 
-                height="350" 
-                src="https://www.youtube.com/embed/S62rpAE_94I" 
-                title="YouTube video player" 
-                frameBorder="0" 
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                allowFullScreen
-                style={iframeStyle}
-              ></iframe>
-              <h4 style={subHeadingStyle}>2023년 대림대 해커톤</h4>
-              <p style={dateStyle}>2023.04 ~ 2023.07</p>
-              <p style={detailStyle}>랜덤 칵테일 추천 - React 프론트 엔드 개발</p>
-            </div>
-          </div>
+   // 현재 필터에 따라 표시할 프로젝트 리스트
+   const filteredProjects = filter === 'personal' ? personalProjects : filter === 'team' ? teamProjects : [...teamProjects, ...personalProjects];
 
-          <div style={projectsContainerStyle}>
-            <div style={activityItemStyle}>
-              <a href="https://www.canva.com/design/DAGJLifg0YI/0ruL7Qk2npEBFaPbBG6IEA/edit?utm_content=DAGJLifg0YI&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" target="_blank" rel="noopener noreferrer">
-                <img src={code} alt="Coding Game Project" style={imageStyle} />
-              </a>
-              
-              <h4 style={subHeadingStyle}>2024년 대림대 해커톤</h4>
-              <p style={dateStyle}>2024.04 ~ 2024.07</p>
-              <a href="https://www.canva.com/design/DAGJLifg0YI/0ruL7Qk2npEBFaPbBG6IEA/edit?utm_content=DAGJLifg0YI&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" target="_blank" rel="noopener noreferrer" style={linkStyle}>프로젝트 소개서</a>
+   return (
+     <section id="projects" style={containerStyle}>
+       <h3 style={headingStyle}>프로젝트</h3>
+ 
+       {/* 필터 버튼 */}
+       <div style={buttonContainerStyle}>
+         <button onClick={() => handleFilterChange('all')} style={{ ...buttonStyle, backgroundColor: filter === 'all' ? '#0056b3' : '#007BFF' }}>
+           모두 보기
+         </button>
+         <button onClick={() => handleFilterChange('team')} style={{ ...buttonStyle, backgroundColor: filter === 'team' ? '#0056b3' : '#007BFF' }}>
+           팀 프로젝트 보기
+         </button>
+         <button onClick={() => handleFilterChange('personal')} style={{ ...buttonStyle, backgroundColor: filter === 'personal' ? '#0056b3' : '#007BFF' }}>
+           개인 프로젝트 보기
+         </button>
+       </div>
+ 
+       {/* 프로젝트 카드 리스트 */}
+       <div style={projectsContainerStyle}>
+         {filteredProjects.map((project, index) => (
+           <div key={index} style={cardStyle}>
+            <h3 style={subHeadingStyle}>{project.title}</h3>
+             {project.images.length > 1 ? (
+               <div style={imageGridStyle}>
+                 {project.images.map((image, i) => (
+                   <img key={i} src={image} alt={`${project.title} - image ${i + 1}`} style={imageStyle} />
+                 ))}
+               </div>
+             ) : (
+               <img src={project.images[0]} alt={project.title} style={imageStyle} />
+             )}
              
+             <p style={projectNameStyle}><strong>프로젝트명 : </strong>{project.projectname}</p>
+             <p style={dateStyle}><strong>기간 : </strong>{project.date}</p>
+             <p style={detailStyle}><strong>팀원 : </strong>{project.person}</p>
+             <p style={detailStyle}><strong>기술스택 : </strong>{project.lan}</p>
+             <p style={detailStyle}><strong>세부사항 : </strong>{project.detail}</p>
+             
+             {project.plus && (
+               <a href={project.plus} target="_blank" rel="noopener noreferrer" style={linkStyle}>
+                 프로젝트 더보기
+               </a>
+             )}
+           </div>
+         ))}
+       </div>
+     </section>
+   );
+ };
+ 
+ // Styles
+ const containerStyle = {
+   padding: '40px 20px',
+   fontFamily: "'Helvetica Neue', Arial, sans-serif",
+   lineHeight: '1.8',
+   backgroundColor: '#fff',
+   color: '#333',
+   maxWidth: '1650px',
+   margin: '0 auto',
+   borderRadius: '10px',
+   boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
+ };
+ 
+ const headingStyle = {
+   borderBottom: '2px solid #007BFF',
+   paddingBottom: '10px',
+   marginBottom: '20px',
+   fontSize: '28px',
+   color: '#333',
+   fontWeight: 'bold',
+ };
 
-              <p style={detailStyle}>코린이들을 위한 코딩 게임 개발 - React 프론트 엔드 개발, Node 백엔드 개발</p>
-            </div>
-
-            <div style={activityItemStyle}>
-              <a href="https://www.miricanvas.com/ko/v/13j287h" target="_blank" rel="noopener noreferrer">
-                <img src={ieum} alt="Ieum Project" style={imageStyle} />
-              </a>
-              
-              <h4 style={subHeadingStyle}>2024년 구름톤 트레이닝 프로젝트</h4>
-              <p style={dateStyle}>2024.05 ~ 2024.08</p>
-              <a href="https://www.miricanvas.com/ko/v/13j287h" target="_blank" rel="noopener noreferrer" style={linkStyle}>프로젝트 소개서</a>
-              <p style={detailStyle}>친구와 함께 여행 계획 웹 - React 프론트 엔드 개발</p>
-            </div>
-          </div>
-
-        </section>
-      </div>
-    </section>
-  );
-};
-
-// Styles
-const containerStyle = {
-  padding: '40px 20px',
-  fontFamily: "'Helvetica Neue', Arial, sans-serif",
-  lineHeight: '1.8',
-  backgroundColor: '#f9f9f9',
-  color: '#333',
-};
-
-const sectionStyle = {
-  marginBottom: '40px',
-  padding: '20px',
+ const buttonContainerStyle = {
+   display: 'flex',
+   justifyContent: 'center',
+   marginBottom: '30px',
+ };
+ 
+ const buttonStyle = {
+   padding: '10px 20px',
+   backgroundColor: '#007BFF',
+   color: '#fff',
+   border: 'none',
+   borderRadius: '20px',
+   cursor: 'pointer',
+   margin: '0 10px',
+   fontSize: '16px',
+   transition: 'background-color 0.3s ease',
+ };
+ 
+ const projectsContainerStyle = {
+   display: 'grid',
+   gridTemplateColumns: 'repeat(2, 1fr)',
+   gap: '30px',
+   padding: '0 20px',
+ };
+ 
+ const cardStyle = {
   backgroundColor: '#fff',
-  borderRadius: '8px',
-  boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
+  padding: '20px',
+  borderRadius: '12px',
+  boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
+  textAlign: 'left',
+  transition: 'transform 0.3s ease, box-shadow 0.3s ease',
+ 
 };
 
-const headingStyle = {
-  borderBottom: '2px solid #007BFF',
-  paddingBottom: '10px',
-  marginBottom: '20px',
-  fontSize: '24px',
-  color: '#333',
-  fontWeight: 'bold',
-};
+ 
+ const subHeadingStyle = {
+   fontSize: '23px',
+   margin: '10px 0',
+   fontWeight: '900',
+   color: '#007BFF'
+ };
+ 
+ const projectNameStyle = {
+   fontSize: '18px',
+   color: '#555',
+   marginBottom: '5px',
+ };
+ 
+ const dateStyle = {
+   fontSize: '16px',
+   color: '#666',
+   marginBottom: '10px',
+ };
+ 
+ const detailStyle = {
+   fontSize: '16px',
+   color: '#555',
+   marginBottom: '15px',
+ };
+ 
+ const linkStyle = {
+   color: '#007BFF',
+   textDecoration: 'none',
+   fontWeight: 'bold',
+   display: 'inline-block',
+   marginTop: '10px',
+ };
 
-const subHeadingStyle = {
-  fontSize: '20px',
-  margin: '10px 0',
-  fontWeight: 'bold',
-  color: '#007BFF',
-};
+ const imageGridStyle = {
+   display: 'grid',
+   gridTemplateColumns: '1fr 1fr',
+   gap: '10px',
+   marginBottom: '15px',
+ };
 
-const projectsContainerStyle = {
-  display: 'flex',
-  flexDirection: 'row', // 가로로 배치
-  gap: '20px',
-  flexWrap: 'wrap', // 내용이 길 경우 다음 줄로 넘김
-};
-
-const activityItemStyle = {
-  flex: '1 1 48%', // 가로로 2개씩 배치되도록 설정
-  marginBottom: '20px',
-  padding: '10px 0',
-};
-
-const dateStyle = {
-  fontSize: '16px',
-  color: '#666',
-};
-
-const detailStyle = {
-  fontSize: '16px',
-  color: '#555',
-};
-
-const iframeStyle = {
-  marginBottom: '20px',
-  borderRadius: '8px',
-};
-
-const imageStyle = {
-  width: '100%',
-  height: '350px',
-  borderRadius: '8px',
-  marginBottom: '10px',
-};
-
-const linkStyle = {
-  color: '#007BFF',
-  textDecoration: 'none',
-  fontWeight: 'bold',
-};
-
-export default Projects;
+ const imageStyle = {
+   width: '100%',
+   height: '400px',
+   objectFit: 'cover',
+   borderRadius: '8px',
+ };
+ 
+ export default Projects;
